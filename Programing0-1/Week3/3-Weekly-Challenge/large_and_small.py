30288c8fe4bd4fa98daf416d9129cc0f834505ae
dinkoslav <dinkoslav@gmail.com>
2015-02-24 23:51:28 +0200

    Week 3 chalange

---
+++ Programing0-1/Week3/3-Weekly-Challenge/large_and_small.py
@@ -0,0 +1,29 @@
+n = input("Enter n: ")
+n = int(n)
+
+def to_digits(n):
+    result = []
+    while n != 0:
+        digit = n % 10
+        result = [digit] + result
+        n = n // 10
+    return result
+
+def to_number(digits):
+    result = 0
+    for digit in digits:
+        result = result * 10 + digit
+    return result
+
+def max_number(n):
+    digits = to_digits(n)
+    digits = sorted(digits, reverse=True)
+    return to_number(digits)
+
+def min_number(n):
+    digits = to_digits(n)
+    digits = sorted(digits)
+    return to_number(digits)
+
+print("Max " + str(max_number(n)))
+print("Min " + str(min_number(n)))
